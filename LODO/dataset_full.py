143362816c93f6281efb6ee28153fc83fcd00216
Hadeel Awwad <120106439+hadiiiil@users.noreply.github.com>
2024-10-09 15:28:40 +0200

    Update dataset_full.py

--- LODO/dataset_full.py
+++ LODO/dataset_full.py
@@ -6,9 +6,9 @@ from adj_matrix_creator import *
 # Generate training Dataset
 for i in range(10): # We have 10 batches of directions  
     dataset_tr(i+1)
-
 dataset_tr_adj_matrix_gen()
 
+# Generate testing Dataset
 for i in range(1): # One single direction
     dataset_ts(i+1)
 dataset_ts_adj_matrix_gen()
